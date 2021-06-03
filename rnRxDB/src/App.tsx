c86dbcc7c428938205ec9d723b4e265bae58642b
Dennis Jordan <dj1042@gmail.com>
2021-06-03 16:53:55 +0100

    Add crew part to UI

--- rnRxDB/src/App.tsx
+++ rnRxDB/src/App.tsx
@@ -13,6 +13,7 @@ import {
 } from 'react-native';
 
 import {Subscription} from 'rxjs';
+import {Crew} from './db/schema/Crew';
 import {Hero} from './db/schema/Hero';
 import {Villain} from './db/schema/Villain';
 import useDatabase from './db/useDatabase';
@@ -22,8 +23,10 @@ const {width} = Dimensions.get('window');
 const App = () => {
   const [heroes, setHeroes] = useState<Hero[]>([]);
   const [villains, setVillains] = useState<Villain[]>([]);
+  const [crews, setCrews] = useState<Crew[]>([]);
   const [heroName, setHeroName] = useState('');
   const [villainName, setVillainName] = useState('');
+  const [crewName, setCrewName] = useState('');
   const {db, loading} = useDatabase();
 
   useEffect(() => {
@@ -41,6 +44,12 @@ const App = () => {
         }
       });
       subs.push(villains_sub);
+      const crew_sub = db.crews.find().$.subscribe(_crews => {
+        if (_crews) {
+          setCrews(_crews);
+        }
+      });
+      subs.push(crew_sub);
     }
 
     return () => {
@@ -56,7 +65,7 @@ const App = () => {
     const color = getRandomColor();
     if (heroName !== '') {
       console.log(`addHero: name: ${heroName}, color: ${color}`);
-      await db.heroes.insert({name: heroName, color: color});
+      await db.heroes.insert({name: heroName, color: color, type: 'Hero'});
       setHeroName('');
     }
   };
@@ -65,8 +74,24 @@ const App = () => {
     const color = getRandomColor();
     if (villainName !== '') {
       console.log(`addVillain: name: ${villainName}, color: ${color}`);
-      await db.villains.insert({name: villainName, color: color});
+      await db.villains.insert({
+        name: villainName,
+        color: color,
+        type: 'Villain',
+      });
       setVillainName('');
+    }
+  };
+
+  const addCrew = async () => {
+    if (crewName !== '') {
+      console.log(`addCrew: name: ${crewName}`);
+      await db.crews.insert({
+        name: crewName,
+        type: 'Crew',
+        members: [],
+      });
+      setCrewName('');
     }
   };
 
@@ -78,6 +103,16 @@ const App = () => {
   const removeVillain = async (villain_name: string) => {
     const found = await db.villains.find().where('name').eq(villain_name);
     await found.remove();
+  };
+
+  const removeCrew = async (crew_name: string) => {
+    const crew_query = await db.crews.findOne().where('name').eq(crew_name);
+    const crew: Crew = await crew_query.exec();
+    crew.members.forEach(async member => {
+      await removeVillain(member);
+      await removeHero(member);
+    });
+    await crew_query.remove();
   };
 
   const getRandomColor = () => {
@@ -93,9 +128,9 @@ const App = () => {
     <SafeAreaView style={styles.topContainer}>
       <StatusBar backgroundColor="#55C7F7" barStyle="light-content" />
       <Text style={styles.title}>Add your favorite hero!</Text>
-      <ScrollView style={styles.heroesList}>
+      <ScrollView style={styles.supersList}>
         <View style={styles.card}>
-          <TouchableOpacity onPress={addHero}>
+          <TouchableOpacity onPress={addCrew}>
             <Image
               style={styles.plusImage}
               source={require('../assets/add.png')}
@@ -103,36 +138,73 @@ const App = () => {
           </TouchableOpacity>
           <TextInput
             style={styles.input}
-            value={heroName}
-            onChangeText={text => setHeroName(text)}
+            value={crewName}
+            onChangeText={text => setCrewName(text)}
           />
         </View>
-        {heroes.length === 0 && <Text>No heroes to display ...</Text>}
-        {heroes.map((hero, index) => (
-          <View style={styles.card} key={index}>
-            <View style={styles.row}>
-              <View
-                style={[
-                  styles.colorBadge,
-                  {
-                    backgroundColor: hero.color,
-                  },
-                ]}
-              />
-              <Text style={styles.heroName}>{hero.name}</Text>
+        {crews.length === 0 && <Text>Create a hero crew first...</Text>}
+        {crews.map((crew, index) => (
+          <View style={styles.crewCard} key={index}>
+            <View style={styles.crewRow}>
+              <View>
+                <Text style={styles.crewName}>{crew.name}</Text>
+              </View>
+              <View>
+                <TouchableOpacity onPress={() => removeCrew(crew.name)}>
+                  <Image
+                    style={styles.minusImage}
+                    source={require('../assets/minus.png')}
+                  />
+                </TouchableOpacity>
+              </View>
             </View>
-            <TouchableOpacity onPress={() => removeHero(hero.name)}>
-              <Image
-                style={styles.plusImage}
-                source={require('../assets/minus.png')}
-              />
-            </TouchableOpacity>
+            <View style={styles.crewSubRow}>
+              <View style={styles.card}>
+                <TouchableOpacity onPress={addHero}>
+                  <Image
+                    style={styles.plusImage}
+                    source={require('../assets/add.png')}
+                  />
+                </TouchableOpacity>
+                <TextInput
+                  style={styles.input}
+                  value={heroName}
+                  onChangeText={text => setHeroName(text)}
+                />
+              </View>
+              {heroes.length === 0 && (
+                <View style={styles.toDisplay}>
+                  <Text>No heroes to display ...</Text>
+                </View>
+              )}
+              {heroes.map((hero, hero_index) => (
+                <View style={styles.card} key={hero_index}>
+                  <View style={styles.row}>
+                    <View
+                      style={[
+                        styles.colorBadge,
+                        {
+                          backgroundColor: hero.color,
+                        },
+                      ]}
+                    />
+                    <Text style={styles.superName}>{hero.name}</Text>
+                  </View>
+                  <TouchableOpacity onPress={() => removeHero(hero.name)}>
+                    <Image
+                      style={styles.minusImage}
+                      source={require('../assets/minus.png')}
+                    />
+                  </TouchableOpacity>
+                </View>
+              ))}
+            </View>
           </View>
         ))}
       </ScrollView>
 
       <Text style={styles.title}>Add your favorite villain!</Text>
-      <ScrollView style={styles.heroesList}>
+      <ScrollView style={styles.supersList}>
         <View style={styles.card}>
           <TouchableOpacity onPress={addVillain}>
             <Image
@@ -146,7 +218,11 @@ const App = () => {
             onChangeText={text => setVillainName(text)}
           />
         </View>
-        {villains.length === 0 && <Text>No villains to display ...</Text>}
+        {villains.length === 0 && (
+          <View style={styles.toDisplay}>
+            <Text>No villains to display ...</Text>
+          </View>
+        )}
         {villains.map((villain, index) => (
           <View style={styles.card} key={index}>
             <View style={styles.row}>
@@ -158,11 +234,11 @@ const App = () => {
                   },
                 ]}
               />
-              <Text style={styles.heroName}>{villain.name}</Text>
+              <Text style={styles.superName}>{villain.name}</Text>
             </View>
             <TouchableOpacity onPress={() => removeVillain(villain.name)}>
               <Image
-                style={styles.plusImage}
+                style={styles.minusImage}
                 source={require('../assets/minus.png')}
               />
             </TouchableOpacity>
@@ -174,6 +250,10 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
+  toDisplay: {
+    marginTop: 10,
+    marginLeft: 10,
+  },
   topContainer: {
     alignItems: 'center',
     backgroundColor: '#55C7F7',
@@ -185,7 +265,7 @@ const styles = StyleSheet.create({
     color: 'white',
     fontWeight: '500',
   },
-  heroesList: {
+  supersList: {
     marginTop: 30,
     borderRadius: 5,
     flex: 1,
@@ -195,8 +275,13 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
   },
   plusImage: {
-    width: 30,
-    height: 30,
+    width: 20,
+    height: 20,
+    marginRight: 15,
+  },
+  minusImage: {
+    width: 25,
+    height: 25,
     marginRight: 15,
   },
   input: {
@@ -206,7 +291,13 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'flex-start',
-
+    marginLeft: 12,
+    paddingVertical: 15,
+    borderBottomColor: '#D2DCE1',
+    borderBottomWidth: 0.5,
+  },
+  crewCard: {
+    justifyContent: 'flex-start',
     marginLeft: 12,
     paddingVertical: 15,
     borderBottomColor: '#D2DCE1',
@@ -216,15 +307,28 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
   },
+  crewRow: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  crewSubRow: {
+    marginLeft: 20,
+  },
   colorBadge: {
     height: 30,
     width: 30,
     borderRadius: 15,
     marginRight: 15,
   },
-  heroName: {
+  superName: {
     fontSize: 18,
     fontWeight: '200',
+    marginTop: 3,
+  },
+  crewName: {
+    fontSize: 20,
+    fontWeight: '300',
     marginTop: 3,
   },
 });
